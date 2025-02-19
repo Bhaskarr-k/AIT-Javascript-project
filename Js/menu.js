c6cb1b1f8141f26c225209e2d2451f5b53a6e142
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-02-19 18:03:00 +0530

    cateogeries in progress

--- Js/menu.js
+++ Js/menu.js
@@ -1,7 +1,17 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px"; /* Show Menu */
+async function fetchCategories() {
+    const response = await fetch('https://www.themealdb.com/api/json/v1/1/categories.php');
+    const data = await response.json();
+    const categoriesDiv = document.getElementById('categories');
+
+    data.categories.forEach(category => {
+        const categoryElement = document.createElement('div');
+        categoryElement.innerHTML = `
+            <h2>${category.strCategory}</h2>
+            <img src="${category.strCategoryThumb}" alt="${category.strCategory}" width="200">
+            
+        `;
+        categoriesDiv.appendChild(categoryElement);
+    });
 }
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0"; /* Hide Menu */
-}
+fetchCategories();
