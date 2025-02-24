478ff77778481dd4d48bd735185afbc6df9cb2f3
Bhaskar <bhaskarkotakonda123@gmail.com>
2025-02-24 10:28:45 +0530

    initial

--- Js/menu.js
+++ Js/menu.js
@@ -1,37 +1,8 @@
-async function fetchData() {
-    try {
-
-    
-        showLoading(true);
-        const mealfinder = await fetch("ttps://www.themealdb.com/api.php");
-        const meal = await mealfinder.json()
-
-        displaymeal(meal);
-
-    }catch{
-        console.error("error fetching data:", error);
-
-    }
-    finally{
-        showLoading(false);
-
+async function randomeal() {
+    try{
+        let response = await fetch("https://www.themealdb.com/api/json/v1/1/categories.php");
+        
     }
 
-
-} 
-function displaymeal(meal) {
-    const  mealcontainer = document.getElementById("categories");
-    mealcontainer.innerHTML = 
-
-
-    meal.forEach(meals => {
-
-        const mealselement = document.createElement("div");
-        mealselement.classList.add("meals");
-
-        const mealsName = document.createElement("h2")
-        mealsName.textContent = meals.name;
-
-
-    })
+   
 }
